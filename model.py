814fafdc16d2833e9272bded389defa72bf1bb2c
Univstar <nixy@qq.com>
2021-12-01 18:30:42 +0800

    remove useless lines

--- model.py
+++ model.py
@@ -1,27 +1,7 @@
-from re import S
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-
-# from legacy.ops import upscale
-
-
-class Network(nn.Module):
-    def __init__(self, shape):
-        super(Network, self).__init__()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(3, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, shape.prod(dtype=torch.int).item()),
-        )
-
-    def forward(self, x):
-        output = self.linear_relu_stack(x)
-        return output
-
 
 class ResModel(nn.Module):
     def __init__(self, conv_num, conv_args, scale=None):
